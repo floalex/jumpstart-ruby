91cd773f2808c6082da04017f309540e18c2086b
floalex <flo.liang@my.com>
2015-08-27 06:51:27 +0000

    Complete encryptor refactoring and decrypt method

--- encryptor.rb
+++ encryptor.rb
@@ -1,20 +1,16 @@
 class Encryptor
-  def cipher
-    { 'a' => 'n', 'b' => 'o', 'c' => 'p', 'd' => 'q', 
-      'e' => 'r', 'f' => 's', 'g' => 't', 'h' => 'u',
-      'i' => 'v', 'j' => 'w', 'k' => 'x', 'l' => 'y',
-      'm' => 'z', 'n' => 'a', 'o' => 'b', 'p' => 'c',
-      'q' => 'd', 'r' => 'e', 's' => 'f', 't' => 'g',
-      'u' => 'h', 'v' => 'i', 'w' => 'j', 'x' => 'k',
-      'y' => 'l', 'z' => 'm'}
+  def cipher(rotation)
+    characters = (' '..'z').to_a
+    rotated_characters = characters.rotate(rotation)
+    Hash[characters.zip(rotated_characters)]
   end
   
-  def encrypt_letter(letter)
-    lowercase_letter = letter.downcase
-    cipher[lowercase_letter]
+  def encrypt_letter(letter, rotation)
+    cipher_for_rotation = cipher(rotation)
+    cipher_for_rotation[letter]
   end
   
-  def encrypt(string)
+  def encrypt(string, rotation)
     # Cut the input string into letters
     letters = string.split("")
     
@@ -22,7 +18,27 @@ class Encryptor
     # After reseraching about .collect and .map method, I decided to use .map method instead of 
     # .collect method in the tutorial, as "The map function has many naming conventions in different       languages"
     results = letters.map do |letter|
-      encrypt_letter(letter)
+      encrypt_letter(letter, rotation)
+    end
+    
+    # Join the results back together in one string
+    results.join
+  end
+  
+  def decrypt_letter(letter, rotation)
+    cipher_for_backrotation = cipher(rotation*(-1))
+    cipher_for_backrotation[letter]
+  end
+  
+  def decrypt(string, rotation)
+    # Cut the input string into letters
+    letters = string.split("")
+    
+    # Encrypt those letters one at a time, gathering the results
+    # After reseraching about .collect and .map method, I decided to use .map method instead of 
+    # .collect method in the tutorial, as "The map function has many naming conventions in different       languages"
+    results = letters.map do |letter|
+      encrypt_letter(letter, rotation*(-1))
     end
     
     # Join the results back together in one string
